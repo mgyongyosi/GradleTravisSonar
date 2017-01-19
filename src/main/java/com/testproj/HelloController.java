4ca38aa9963b69adf8c8b0706c1af0acd3e2317b
mgyongyosi <mgyongyosi@users.noreply.github.com>
2017-01-19 22:42:30 +0100

    added some smells

--- src/main/java/com/testproj/HelloController.java
+++ src/main/java/com/testproj/HelloController.java
@@ -11,6 +11,13 @@ public class HelloController {
 
     @RequestMapping("/")
     public String hello(){
-        return "Hello World";
+        return "Hello World" + MyMethodWithManyParameters(4,2,1,"oops,", "it's long", 456L);
+    }
+
+    private String MyMethodWithManyParameters(int aaaaa, int bbbbbb, int c, String oooh, String my, long parameterList){
+        int other = 0;
+        int intSum = aaaaa + bbbbbb + c;
+        String concatedString = oooh + my + (parameterList + intSum);
+        return concatedString;
     }
 }
